3f91edbef9f3be2d176482d30bad85e44c9d0a4b
Maksimka <147372430+maksimka-ui@users.noreply.github.com>
2024-09-22 18:06:42 +0300

    Add files via upload

---
+++ Lab01_01/Lab01_01.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Lab01_01
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double z1 = Math.Sqrt(2 * b + 2 * Math.Sqrt(Math.Pow(b, 2) - 4)) / Math.Sqrt(Math.Pow(b, 2) - 4 + b + 2);
+        double z2 = 1 / (Math.Sqrt(b + 2));
+        Console.WriteLine("Думаю правильно посчитал :) ");
+        Console.WriteLine($"z1 = {z1}");
+        Console.WriteLine($"z2 = {z2}");
+        Console.ReadLine();
+    }
+    
+}
